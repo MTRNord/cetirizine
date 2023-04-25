0038eb3e299122741c431ed972d11e29c921a1f6
MTRNord <MTRNord@users.noreply.github.com>
2023-04-25 22:21:03 +0000

    Deploying to gh-pages from @ MTRNord/cetirizine@e4a85f31a207d042790c61b81ad95fe607ce8bca 🚀

---
+++ storybook/assets/button.stories-dc220268.js
@@ -0,0 +1,44 @@
+import{B as c}from"./button-e6a9a480.js";import{e as i}from"./index-9ff52a90.js";import"./jsx-runtime-ccada58e.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";const y={title:"Fundamentals/Buttons/BasicButton",tags:["autodocs"],component:c,parameters:{badges:[i.EXPERIMENTAL]},argTypes:{children:{required:!0,name:"Label",description:"The button label"},onClick:{description:"The button onClick handler",required:!0},type:{description:"The button type",control:"select",options:["button","submit","reset"],defaultValue:"button"},style:{description:"The button styling",control:"select",options:["primary","secondary","abort"],defaultValue:"primary"}}},e={args:{children:"Click me",type:"button",style:"primary"}},r={args:{children:"Submit",type:"submit",style:"primary"}},t={args:{children:"Reset",type:"reset",style:"primary"}},s={args:{children:"Click me",type:"button",style:"secondary"}},a={args:{children:"Submit",type:"submit",style:"secondary"}},o={args:{children:"Reset",type:"reset",style:"secondary"}},n={args:{children:"Abort",type:"button",style:"abort"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Click me',
+    type: 'button',
+    style: 'primary'
+  }
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Submit',
+    type: 'submit',
+    style: 'primary'
+  }
+}`,...r.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Reset',
+    type: 'reset',
+    style: 'primary'
+  }
+}`,...t.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Click me',
+    type: 'button',
+    style: 'secondary'
+  }
+}`,...s.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Submit',
+    type: 'submit',
+    style: 'secondary'
+  }
+}`,...a.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Reset',
+    type: 'reset',
+    style: 'secondary'
+  }
+}`,...o.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Abort',
+    type: 'button',
+    style: 'abort'
+  }
+}`,...n.parameters?.docs?.source}}};const b=["Default","Submit","Reset","DefaultSecondary","SubmitSecondary","ResetSecondary","Abort"];export{n as Abort,e as Default,s as DefaultSecondary,t as Reset,o as ResetSecondary,r as Submit,a as SubmitSecondary,b as __namedExportsOrder,y as default};
+//# sourceMappingURL=button.stories-dc220268.js.map
