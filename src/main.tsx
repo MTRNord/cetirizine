7f6d080dba2dba4a5c219b15720cdd5e9d451036
MTRNord <mtrnord1@gmail.com>
2023-05-08 01:50:42 +0200

    Fix unused imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { LoggerLevel, Tracing, initAsync, start } from "@mtrnord/matrix-sdk-crypto-js";
+import { initAsync, start } from "@mtrnord/matrix-sdk-crypto-js";
 import React from 'react'
 import { createRoot } from 'react-dom/client';
 import './index.scss';
