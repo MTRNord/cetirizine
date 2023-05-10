3d00fac499dd122eef5aedfcf286144975101bf9
MTRNord <mtrnord1@gmail.com>
2023-05-10 17:15:27 +0200

    Shake the tree

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,20 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), splitVendorChunkPlugin()],
+    plugins: [
+        react(),
+        splitVendorChunkPlugin()
+    ],
     appType: 'mpa',
     build: {
-        target: 'esnext'
+        target: 'esnext',
+        rollupOptions: {
+            treeshake: 'recommended',
+        }
     },
+    // Broken
+    // optimizeDeps: {
+    //     disabled: false,
+    //     exclude: []
+    // }
 })
